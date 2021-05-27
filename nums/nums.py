c1ff150761c74bfb81f16d424f6f5ab990486539
armen <armen@sap.su>
2021-05-27 16:16:40 +0300

    add multi minus

--- nums/nums.py
+++ nums/nums.py
@@ -1,6 +1,8 @@
 def plus(a, b):
     return a + b
 
+def multi(a, b):
+    return a*b
 
 def minus(a, b):
     return a - b
@@ -8,3 +10,5 @@ def minus(a, b):
 text = "new test"
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(multi(2, 2))
+    print(minus(2, 2))
